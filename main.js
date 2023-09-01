24d3a4da7036abbc9da1aceb9d2193da1e583b00
bravere0129 <samkowork@mail.ru>
2023-09-01 16:23:29 +0300

    second commit

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ const bot = mineflayer.createBot({
   port: '33415',
   username: 'Annihilator'
 })
+
+/* -------------------------------------------------------------------------- */
+/*                        бот будет смотреть на игрока                        */
+/* -------------------------------------------------------------------------- */
+
+function lookAtNearestPlayer () {
+  const playerFilter = (entity) => entity.type === 'player'
+  const playerEntity = bot.nearestEntity(playerFilter)
+
+  if (!playerEntity) return
+
+  const pos = playerEntity.position.offset(0, playerEntity.height, 0)
+  bot.lookAt(pos)
+}
+
+bot.on('physicTick',lookAtNearestPlayer)
